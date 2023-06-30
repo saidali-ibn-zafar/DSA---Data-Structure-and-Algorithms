7eb11090f8b828862c640a23ed63fa32204279bf
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-06-30 23:48:19 +0200

    Update knowBasicMath.js

--- DSA/knowBasicMath.js
+++ DSA/knowBasicMath.js
@@ -12,4 +12,51 @@ const evenlyDivide = (num) => {
 
 console.log(evenlyDivide(36));
 
-// - - - - - - - - - - 
+// = = = = =
+
+// Reverse a number
+const reverseNnumber = (num) => {
+  const res = num.toString().split("").reverse().join("");
+  return Number(res);
+};
+
+console.log(reverseNnumber(123));
+
+// = = = = =
+
+// Check if a number is Palindrome or Not
+const isPalindromeNum = (num) => {
+  const res = num.toString().split("").reverse().join("");
+  return Number(res) === num;
+};
+
+console.log(isPalindromeNum(123));
+
+// = = = = =
+
+// Check if a number is Armstrong Number or not
+const isArmstrongNumber = (num) => {
+  const arr = Array.from(num.toString(), Number);
+  var len = arr.length;
+  let sum = 0;
+  for (let i = 0; i < len; i++) {
+    sum += Math.pow(arr[i], len);
+  }
+
+  return sum === num;
+};
+
+console.log(isArmstrongNumber(153));
+
+// = = = = =
+
+// = = = = =
+
+// = = = = =
+
+// = = = = =
+
+// = = = = =
+
+// = = = = =
+
